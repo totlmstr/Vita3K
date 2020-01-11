da568aab965c59512069fc3a5e196935a289a635
totlmstr <totlmstr@gmail.com>
2020-01-11 00:09:42 -0800

    Vita3K: Refactor CMake to optionally build Boost
    This implements a fallback for C++11 builds. Travis for Mac is not yet updated to C++17 (minimum is macOS 10.15, and no experimental), so this fallback is needed.

--- gen-windows.bat
+++ gen-windows.bat
@@ -5,17 +5,6 @@ choice /c 79 /n /t 3 /d 7 /m "Press '7' for VS 2017 or '9' for VS 2019: "
 if errorlevel 1 set vs_version=Visual Studio 15 2017 Win64
 if errorlevel 2 set vs_version=Visual Studio 16 2019
 
-REM CI uses pre-built Boost
-IF "%CI%"=="" IF NOT EXIST external\boost-build (
-	REM Create build dir
-	mkdir external\boost-build
-	cd external\boost
-
-	REM Build our Boost subset
-	b2 -j5 --build-dir=../boost-build --stagedir=../boost-build toolset=msvc stage
-	cd ../..
-)
-
 REM Create build folder
 mkdir build-windows
 pushd build-windows
